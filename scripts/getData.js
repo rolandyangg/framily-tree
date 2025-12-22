e2d0d4718ac69f358d6836bea7e68493ca765591
roland yang <rolandyang@gmail.com>
2025-12-22 03:29:54 -0500

    new spreadsheet id

--- scripts/getData.js
+++ scripts/getData.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 // Forking instructions: if you are forking this project for your own
 // fraternity, you will need to change the SPREADSHEET_ID to match the URL of
 // your new Google spreadsheet:
-var SPREADSHEET_ID = '1h6dVJKtETWX3Kr9PT6EaLu0gGavdi8Gnj4IlX155pfY';
+var SPREADSHEET_ID = '1B-IacGvDY-EUJuF_tDhElA4QoNRlNIPbTNtQIkoEi-c';
 
 var apiKey;
 try {
